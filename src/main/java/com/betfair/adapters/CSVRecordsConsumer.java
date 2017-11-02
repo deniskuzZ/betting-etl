6087568f493e65cffd35cf8c73b46ff7e4ebdbd5
Denys Kuzmenko <Denys_Kuzmenko@epam.com>
2017-11-02 10:05:42 +0100

    small refactor

--- src/main/java/com/betfair/adapters/CSVRecordsConsumer.java
+++ src/main/java/com/betfair/adapters/CSVRecordsConsumer.java
@@ -27,21 +27,20 @@ public class CSVRecordsConsumer implements RecordsConsumer {
     @Autowired
     private StreamFactory factory;
 
-    public Map<Tuple2<String, Currency>, Tuple3<Long, Double, Double>> apply(Function<Stream<BettingRecord>, Callable<Map<Tuple2<String, Currency>, Tuple3<Long, Double, Double>>>> function) throws IOException, ExecutionException, InterruptedException {
-        BeanReader in = factory.createReader("betting-records",
-                new File("/Users/kuzmende/projects/betting-etl/input.csv"));
+    private ThreadLocal<Unmarshaller> unmarshaller = ThreadLocal.withInitial(
+            () -> factory.createUnmarshaller("betting-records"));
 
+    public Map<Tuple2<String, Currency>, Tuple3<Long, Double, Double>> apply(Function<Stream<BettingRecord>, Callable<Map<Tuple2<String, Currency>, Tuple3<Long, Double, Double>>>> function) throws IOException, ExecutionException, InterruptedException {
         File inputF = new File("/Users/kuzmende/projects/betting-etl/input.csv");
+        InputStream inputFS = new FileInputStream(inputF);
 
         Callable<Map<Tuple2<String, Currency>, Tuple3<Long, Double, Double>>> task;
         Map<Tuple2<String, Currency>, Tuple3<Long, Double, Double>> result;
 
-        InputStream inputFS = new FileInputStream(inputF);
-
         try (BufferedReader br = new BufferedReader(new InputStreamReader(inputFS))) {
             task = function.apply(
                     br.lines().skip(1).parallel().map(t -> (BettingRecord)
-                            factory.createUnmarshaller("betting-records").unmarshal(t)));
+                            unmarshaller.get().unmarshal(t)));
 
             ForkJoinPool forkJoinPool = new ForkJoinPool(4);
             result = forkJoinPool.submit(task).get();
